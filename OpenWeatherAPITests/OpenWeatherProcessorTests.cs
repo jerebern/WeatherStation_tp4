a626ab7b3cf1ed88c8b26e1d10bff47d7b3bc26b
Jérémy Bernard <jere.bern@hotmail.com>
2020-11-15 20:04:27 -0500

    2. Création des tests

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -1,9 +1,41 @@
-﻿using System;
+﻿using OpenWeatherAPI;
+using System;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace OpenWeatherAPITests
 {
     public class OpenWeatherProcessorTests
     {
+        private OpenWeatherProcessor _sut;
+        //o	Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
+        [Fact]
+        public async Task GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            _sut = OpenWeatherProcessor.Instance;
+            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetOneCallAsync());
+        }
+        //o	Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
 
+        [Fact]
+        public async Task GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            _sut = OpenWeatherProcessor.Instance;
+            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetCurrentWeatherAsync());
+        }
+        //o	Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
+        [Fact]
+        public async Task GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            _sut = OpenWeatherProcessor.Instance;
+            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetOneCallAsync());
+        }
+        //o	Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
+        [Fact]
+        public async Task GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            _sut = OpenWeatherProcessor.Instance;
+            await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetCurrentWeatherAsync());
+        }
     }
 }
